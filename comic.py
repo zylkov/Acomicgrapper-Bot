0d6da0bef51a39596d10491eef3168c2941b265d
Pixel-Tentacles <zylkov2010@gmail.com>
2019-08-08 17:30:16 +0300

    get info translation comic

--- comic.py
+++ comic.py
@@ -3,13 +3,47 @@ from bs4 import BeautifulSoup
 
 url=r"https://acomics.ru/"
 
+type_info ={
+    "Автор оригинала:":"authors",
+    "Авторы оригинала:":"authors",
+    "Переводчики:":"authors_translation",
+    "Переводчик:":"authors_translation",
+    "Количество выпусков:":"count_pages",
+    "Количество подписчиков:":"count_subscribes",
+    "Официальный сайт:":"offical_site",
+    "Возрастной рейтинг:":"rating",
+    "Лицензия:":"license"
+}
+
+def remove_tag(select):
+    if select.name is not None: 
+        return select.text
+    return select
+
 def get_comic(name):
     page = requests.get(url+'~'+name+"/about")
     soup = BeautifulSoup(page.text, 'html.parser')
-    print(page)
+    content = soup.find(id="contentMargin")
+    data={}
+    data["name"] = name
+    data["title"] = content.find("h2").text
+    
+    about_summary = content.find("div", class_="about-summary")
+    #Добавить исключение KEYERROR у получения инофрмации у комикса
+    for info in about_summary.find_all("p"):
+        if len(info.contents) > 1 and info.b is not None:
+            # индекс определяеться по славарю
+            name_data = type_info[info.contents[0].text.strip()]
+            # у данных все теги удалються и в дату записываеться строка
+            info_withaut_tag = list(map(remove_tag, info.contents[1:]))
+            data[name_data] = "".join([i.strip() for i in info_withaut_tag])
+    
+    
+    return data
+
 
 def main():
-    get_comic("randowis")
+    print(get_comic("DarkForestOnline"))
 
 
 if __name__ == '__main__':
