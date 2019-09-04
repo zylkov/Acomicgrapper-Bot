aa4f334ac4160e842ba3a0f6e506826fddeda5c2
zylkov <zylkov.work@gmail.com>
2019-09-04 16:51:15 +0300

    test bot oop

--- main.py
+++ main.py
@@ -8,34 +8,79 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
+class TestBoop:
+    def __init__(self):
+        self.TOKEN = os.environ.get('TOKEN', None)
+        self.NAME = os.environ.get('NAME', None)
+        # Port is given by Heroku
+        self.PORT = os.environ.get('PORT')
+
+        # Enable logging
+        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                            level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+
+        #State 
+        self.click = 0
+        # Set up the Updater
+        updater = Updater(TOKEN)
+        dp = updater.dispatcher
+        # Add handlers
+
+        dp.add_handler(CommandHandler('start', scenes.start))
+        dp.add_handler(CommandHandler('help', scenes.help))
+        dp.add_handler(CommandHandler('test', scenes.test))
+        dp.add_handler(CommandHandler('boop', self.boop))
+
+        # dp.add_handler(MessageHandler(Filters.text, echo))
+        dp.add_error_handler(error)
+
+        # Start the webhook
+        updater.start_webhook(listen="0.0.0.0",
+                            port=int(PORT),
+                            url_path=TOKEN)
+        updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+        updater.idle()
+        
+    
+    def boop(self, update, context):
+        self.click += 1
+        chat_id = update.message.chat_id
+        bot = context.bot
+        bot.send_message(chat_id=chat_id, text="Boop:{}".format(self.click))
+        
+
 
 if __name__ == "__main__":
-    # Set these variable to the appropriate values
-    TOKEN = os.environ.get('TOKEN', None)
-    NAME = os.environ.get('NAME', None)
+    # # Set these variable to the appropriate values
+    # TOKEN = os.environ.get('TOKEN', None)
+    # NAME = os.environ.get('NAME', None)
 
-    # Port is given by Heroku
-    PORT = os.environ.get('PORT')
+    # # Port is given by Heroku
+    # PORT = os.environ.get('PORT')
 
-    # Enable logging
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
-    logger = logging.getLogger(__name__)
+    # # Enable logging
+    # logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    #                     level=logging.INFO)
+    # logger = logging.getLogger(__name__)
 
-    # Set up the Updater
-    updater = Updater(TOKEN)
-    dp = updater.dispatcher
-    # Add handlers
-    dp.add_handler(CommandHandler('start', scenes.start))
-    dp.add_handler(CommandHandler('help',scenes.help))
-    dp.add_handler(CommandHandler('test',scenes.test))
+    # # Set up the Updater
+    # updater = Updater(TOKEN)
+    # dp = updater.dispatcher
+    # # Add handlers
 
-    # dp.add_handler(MessageHandler(Filters.text, echo))
-    dp.add_error_handler(error)
+    # dp.add_handler(CommandHandler('start', scenes.start))
+    # dp.add_handler(CommandHandler('help',scenes.help))
+    # dp.add_handler(CommandHandler('test',scenes.test))
 
-    # Start the webhook
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
-    updater.idle()
+    # # dp.add_handler(MessageHandler(Filters.text, echo))
+    # dp.add_error_handler(error)
+
+    # # Start the webhook
+    # updater.start_webhook(listen="0.0.0.0",
+    #                       port=int(PORT),
+    #                       url_path=TOKEN)
+    # updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+    # updater.idle()
+
+    bot = TestBoop()
--- scenes.py
+++ scenes.py
@@ -6,11 +6,17 @@ def get_response_text(file_name):
     path = '/'.join(('texts', file_name))
     return pkg_resources.resource_string(__name__, path).decode("UTF-8")
 
+def get_message_text(name_file, args={}):
+    get_response_text(name_file).format(args)
+
 def start(bot, update):
-    update.effective_message.reply_text(text=get_response_text('greeting.tg.md').format(update.message.from_user.first_name), parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
+    update.effective_message.reply_text(text=get_message_text('greeting.tg.md', update.message.from_user.first_name), parse_mode=telegram.ParseMode.MARKDOWN)
 
 def help(bot, update):
-    update.effective_message.reply_text(text=get_response_text('help.tg.md'), parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
+    update.effective_message.reply_text(text=get_message_text('help.tg.md'), parse_mode=telegram.ParseMode.MARKDOWN)
+
+def searchcat(bot, update):
+    update.effective_message.reply_text(text=get_message_text("searchcat.tg.md"),parse_mode=telegram.ParseMode.MARKDOWN)
 
 def test(bot, update):
     text = "*bold* _italic_ `fixed width font` [link](http://google.com)."
